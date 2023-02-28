266a1d8e8ce7489eb6052e2b5524d52b116a457b
Mohamed Amine Safi <mohammedameenalsafi@gmail.com>
2023-02-28 10:29:46 +0100

    Menu of Child Page Links

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -34,13 +34,38 @@ while (have_posts()) {
         <?php }
         ?>
 
-        <!-- <div class="page-links"> -->
-        <!-- <h2 class="page-links__title"><a href="#">About Us</a></h2> -->
-        <!-- <ul class="min-list"> -->
-        <!-- <li class="current_page_item"><a href="#">Our History</a></li> -->
-        <!-- <li><a href="#">Our Goals</a></li> -->
-        <!-- </ul> -->
-        <!-- </div> -->
+        <?php
+        // This is used to make sure that the page has children
+        $testArray = get_pages(
+            array(
+                'child_of' => get_the_ID()
+            )
+        );
+
+        if ($theParent or $testArray) {
+            ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent) ?></a></h2>
+                <ul class="min-list">
+                    <?php
+                    if ($theParent) {
+                        $findChildOf = $theParent;
+                    } else {
+                        $findChildOf = get_the_ID();
+                    }
+
+                    wp_list_pages(
+                        array(
+                            'title_li' => NULL,
+                            'child_of' => $findChildOf,
+                            'sort_column' => 'menu_order'
+                        )
+                    );
+                    ?>
+                </ul>
+            </div>
+        <?php }
+        ?>
 
         <div class="generic-content">
             <?php the_content(); ?>
